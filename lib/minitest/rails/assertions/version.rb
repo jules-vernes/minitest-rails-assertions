413f4b1839c7ac1e5b036ddce1d27e7421e563db
Geoffrey Roguelon <geoffrey.roguelon@gmail.com>
2014-09-08 13:39:51 +0200

    Update the version to 0.0.1.alpha1.

--- lib/minitest/rails/assertions/version.rb
+++ lib/minitest/rails/assertions/version.rb
@@ -7,7 +7,7 @@ module Minitest
         MAJOR = 0
         MINOR = 0
         TINY  = 1
-        PRE   = 'dev'
+        PRE   = 'alpha1'
 
         STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
       end
